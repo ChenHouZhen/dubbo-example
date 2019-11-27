a52b02e6205b10c5b13b7bc42a59cdc1f7281c26
chenhouzhen <chenhouzhen@hqjy.com>
2019-11-27 17:10:45 +0800

    log

--- dubbo-provider/src/main/java/com/chenhz/provider/service/KnowledgeServiceImpl.java
+++ dubbo-provider/src/main/java/com/chenhz/provider/service/KnowledgeServiceImpl.java
@@ -1,8 +1,9 @@
 package com.chenhz.provider.service;
 
 import com.chenhz.api.KnowledgeService;
+import lombok.extern.slf4j.Slf4j;
 
-//@Slf4j
+@Slf4j
 @com.alibaba.dubbo.config.annotation.Service
 public class KnowledgeServiceImpl implements KnowledgeService {
 
